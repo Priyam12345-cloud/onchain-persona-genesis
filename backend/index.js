08c0b5b1608512cdc70d53a4bf0d607d7f5a8910
Thunder25Beast <147324100+Thunder25Beast@users.noreply.github.com>
2025-05-30 16:57:50 +0530

    backend update

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import { spawn } from 'child_process';
 import fs from 'fs/promises';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
